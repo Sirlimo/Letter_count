c3dc3985c69fbe46d92f8f230ff5d94810fa3eb2
sirlimo <sirlimo92gmail.com>
2020-07-07 17:05:22 -0700

    fix of letter_count.py

--- letter_count.py
+++ letter_count.py
@@ -4,11 +4,14 @@ def letter_count(argv):
     f = open(argv, "r")
     cpt = 0
     line = f.readline()
-    while 
-    for x in line:
-        if (x >= 'a' and x <= 'z') or (x >= 'A' and x <= 'Z'):
-            cpt += 1
-    close(f)
+    while line:
+        if line == "":
+            break
+        for x in line:
+            if (x >= 'a' and x <= 'z') or (x >= 'A' and x <= 'Z'):
+                cpt += 1
+        line = f.readline()
+    f.close()
     return cpt
 
 def main(argv):
